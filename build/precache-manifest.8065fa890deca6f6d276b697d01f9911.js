738333c4f8c8ffc9d139b0da65035571165a5fcf
Bryan Law <bryanwal6@gmail.com>
2020-07-31 01:38:08 -0400

    build

---
+++ build/precache-manifest.8065fa890deca6f6d276b697d01f9911.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3b846a3fb29c41b3ad4035dc7f5ee8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d16e0c81878c9ceda88",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6121418fbbb9cb6eb1ff",
+    "url": "/static/css/main.33b06bab.chunk.css"
+  },
+  {
+    "revision": "8d16e0c81878c9ceda88",
+    "url": "/static/js/2.ddc273b4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ddc273b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6121418fbbb9cb6eb1ff",
+    "url": "/static/js/main.268162dd.chunk.js"
+  },
+  {
+    "revision": "0a2964fa4fb90d7b6b45",
+    "url": "/static/js/runtime-main.98b3d972.js"
+  }
+]);
